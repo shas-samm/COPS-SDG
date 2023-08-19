bab7179c5201707584df9240fd3a8de913bd25ea
shas-samm <122719642+shas-samm@users.noreply.github.com>
2023-08-19 18:49:56 +0530

    Commit 2 on Home.js

--- Home.js
+++ Home.js
@@ -1,19 +1,38 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import { NavLink } from "react-router-dom";
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 const Home = () => {
     return (
 <>
-{/* <div className="d-flex justify-content-around"> */}
-<h1>SPORTS SCHEDULES</h1>
-    <hr/>
-{/* </div> */}
-<Button variant="primary">CRICKET</Button>
-<Button variant="success">FOOTBALL</Button>
-<Button variant="warning">HOCKEY</Button>
-<Button variant="dark">BASKETBALL</Button>
+<h2>Select your favourite sport</h2>
+<div className="schedule-tab container">
+  <div className="schedule-tab justify-content-around">
+  <button className="btn btn-primary">CRICKET</button>
+  <button className="btn btn-success">FOOTBALL</button>
+  <button className="btn btn-dark">HOCKEY</button>
+  <button className="btn btn-warning">BASKETBALL</button>
+  </div>
+</div>
+<Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+      </ListGroup>
+      <Card.Body>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
 </>
   )
 }
